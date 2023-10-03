99d21eee9e4c83963756c372342af471c76f03cb
Fares Ahmed <fareselzeny6@gmail.com>
2023-10-04 00:14:42 +0300

    power

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def pow(a, b):
+    c = a
+    if b == 0:
+        return 1
+    elif b > 0:
+        for i in range(b - 1):
+            c = c * a
+        return c
+    else:
+        for i in range(abs(b) - 1):
+            c = c * a
+        c = float(1 / c)
+        return c

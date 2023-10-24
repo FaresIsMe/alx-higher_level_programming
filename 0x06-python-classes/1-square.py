bea22d8a15130f9652d5ebcf18b0607f5797f6e3
Fares Ahmed <fareselzeny6@gmail.com>
2023-10-24 13:40:56 +0300

    [Add] -1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """Square represtnation."""
+    def __init__(self, size):
+        """Initialze a new Square.
+
+        Args:
+        size(int): The size of the square
+        """
+
+        self.__size = size

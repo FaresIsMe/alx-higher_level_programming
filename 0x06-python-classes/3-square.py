f59ecf8266f79ce5107067173c564c3bb6fe93e9
Fares Ahmed <fareselzeny6@gmail.com>
2023-10-24 15:26:44 +0300

    [Add]

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -19,3 +19,11 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+
+    def area(self):
+        """Square area
+        Args:
+            a(int): Are of the square
+        """
+        a = self.__size * self.__size
+        return a
---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """Square represtnation."""
+
+    def __init__(self, size=0):
+        """Initialze a new Square.
+
+        Args:
+            size(int): The size of the square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Gets the size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+
+        if not isinstance(value, int):
+            raise TypeError("size must be integer")
+
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Square area
+        Args:
+            a(int): Are of the square
+        """
+        a = self.__size * self.__size
+        return a

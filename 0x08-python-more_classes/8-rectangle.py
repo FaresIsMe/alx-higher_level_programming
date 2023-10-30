2b534c7262a105bfcc746b804162038bd2b4d38a
Fares Ahmed <fareselzeny6@gmail.com>
2023-10-30 20:21:03 +0200

    [Add] bigger or equal

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -85,6 +85,7 @@ class Rectangle:
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
 
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """Checks which rectangle is bigger"""
         if type(rect_1) is not Rectangle:

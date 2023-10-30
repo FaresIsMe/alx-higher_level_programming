2ad9cb3f700c5449fbb4c0a2f1f12c8b148b2769
Fares Ahmed <fareselzeny6@gmail.com>
2023-10-30 17:30:25 +0200

    [Add] -width and height

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Define a class Rectangle"""
+
+
+class Rectangle:
+    """Rectangle represtnation"""
+    def __init__(self, width=0, height=0):
+        """Initialize a new rectangle
+        
+        Args:
+            height
+            width
+        """
+        self.height = height
+        self.width = width
+        
+        
+    @property
+    def width(self):
+        """Set the width"""
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        if type(value) is int:
+            if value >= 0:
+                self.__width = value
+            else:
+                raise ValueError("width must be >= 0")
+        else:
+            raise TypeError("width must be an integer")
+        
+    @property
+    def height(self):
+        """Set the height"""
+        return self.__height
+    
+    @width.setter
+    def height(self, value):
+        if type(value) is int:
+            if value >= 0:
+                self.__height = value
+            else:
+                raise ValueError("height must be >= 0")
+        else:
+            raise TypeError("height must be an integer")
+        
+        

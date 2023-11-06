5c7838fe762c53b79d821d6d5664c470f580567b
Fares Ahmed <fareselzeny6@gmail.com>
2023-11-06 18:58:08 +0200

    [Add]

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+=====================
+Module to check class
+=====================
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """is same class?"""
+    return isinstance(obj, a_class)

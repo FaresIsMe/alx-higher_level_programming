dc04c27f9dfb7791431c80a5be621976704a30ab
Fares Ahmed <fareselzeny6@gmail.com>
2023-11-06 19:15:00 +0200

    useless step

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+=========================
+Define class BaseGeometry
+=========================
+"""
+
+
+class BaseGeometry(object):
+    """Class representaion"""
+    pass
+
+    def area(self):
+        """It raises and exception"""
+        raise Exception("area() is not implemented")

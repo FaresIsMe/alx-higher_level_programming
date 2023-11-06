d3094f1a495cff202f6a909aad55db7cd1851358
Fares Ahmed <fareselzeny6@gmail.com>
2023-11-06 18:13:50 +0200

    [Add]"

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+=====================
+Module to check class
+=====================
+"""
+
+
+def is_same_class(obj, a_class):
+    """is same class?"""
+    return type(obj) is a_class

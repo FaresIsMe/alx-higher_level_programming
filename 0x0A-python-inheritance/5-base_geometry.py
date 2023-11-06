1942085390d7edef80938d1ee2e5a07918005e26
Fares Ahmed <fareselzeny6@gmail.com>
2023-11-06 19:07:28 +0200

    Class BaseGeometry

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+=========================
+Define class BaseGeometry
+=========================
+"""
+
+class BaseGeometry(object):
+    """Class representaion"""
+    pass

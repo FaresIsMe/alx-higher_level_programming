45fe365e76b27133ca4a04429c0182117a458b5d
Fares Ahmed <fareselzeny6@gmail.com>
2023-11-06 17:40:58 +0200

    Lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Module with the method lookpu
+"""
+
+
 def lookup(obj):
     """ returns the list of available attributes and methods of an object"""
     return sorted(dir(obj))

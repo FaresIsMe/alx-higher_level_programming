7f22249871aea0658a7dae8b60efed589771fb53
Fares Ahmed <fareselzeny6@gmail.com>
2023-11-06 20:02:46 +0200

    [Add]

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
-                
+
 """
 ============================
 Define child class Rectangle
@@ -10,7 +10,7 @@ Define child class Rectangle
 
 class Rectangle(BaseGeometry):
     """Class Representaion"""
-    
+
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.__width = width

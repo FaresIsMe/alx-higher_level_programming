ee8b09857eed54fdb99ad98714a1c79c23580437
Fares Ahmed <fareselzeny6@gmail.com>
2023-11-06 20:39:42 +0200

    [Add]

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+===================
+Square Class Module
+===================
+"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Square Class"""
+
+    def __init__(self, size):
+        """Initilize Square method"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)

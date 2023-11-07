65e72fcd417dac9f0fbd66e3ab9dc5b309285232
Fares Ahmed <fareselzeny6@gmail.com>
2023-11-07 19:45:47 +0200

    JSON

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -7,4 +7,4 @@ Define an append module
 def append_write(filename="", text=""):
     """Appends on a file with UTF-8"""
     with open(filename, mode='a', encoding="UTF-8") as file:
-        return (file.write(text))
+        return file.write(text)
---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import json
+"""
+I wallahy don't know what is this
+"""
+
+
+def to_json_string(my_obj):
+    """I swear if this works Imma be gased!"""
+    x = json.dumps(my_obj)
+    return x

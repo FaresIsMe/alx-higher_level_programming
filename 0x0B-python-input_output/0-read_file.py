67d6acf2d37accb24c9430f5a8db04fd05fd620e
Fares Ahmed <fareselzeny6@gmail.com>
2023-11-07 18:17:48 +0200

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+"""
+defining read_file function
+"""
 
 
 def read_file(filename=""):
+    """reads the file with utf-8"""
     with open(filename, 'r',  encoding="UTF-8") as file:
         read_data = file.read()
-    print(read_data)
+    print(read_data, end='')

2e39465c31630074a3a17b4f0f0a22e274a7cc3a
Fares Ahmed <fareselzeny6@gmail.com>
2023-11-07 20:21:28 +0200

    json

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+I wallahy don't know what is this
+"""
+import json
+
+
+def from_json_string(my_str):
+    """I swear if this works Imma be gased!"""
+    x = json.loads(my_str)
+    return x

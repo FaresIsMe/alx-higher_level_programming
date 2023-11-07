db38254e0ced7e44ad5aeb0cd7babe65db6eafa3
Fares Ahmed <fareselzeny6@gmail.com>
2023-11-07 19:30:08 +0200

    Append

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Define an append module
+"""
+
+
+def append_write(filename="", text=""):
+    """Appends on a file with UTF-8"""
+    with open(filename, mode='a', encoding="UTF-8") as file:
+        return (file.write(text))

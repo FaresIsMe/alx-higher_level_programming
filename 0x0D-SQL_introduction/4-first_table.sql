4289c1e48cb2525b4ef925628a054a705f2a198f
Fares Ahmed <fareselzeny6@gmail.com>
2023-12-13 02:36:49 +0200

    Craete first table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,5 @@
--- This script creats a table in MySQL server
+-- This script creats a table in database
+-- -- Exceute: cat 4-first_table.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
 CREATE TABLE IF NOT EXISTS first_table(
 	id INT,
 	name VATCHAR(256),
